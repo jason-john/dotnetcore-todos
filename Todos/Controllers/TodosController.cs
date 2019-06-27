623eafe57184738904cf0ef332f0b7b3158421f7
jason-john <jasonjohn12@gmail.com>
2019-06-27 13:26:41 -0500

    Created temporary data store and Get endpoint for todos

---
+++ Todos/Controllers/TodosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todos.Controllers
+{
+    [Route("api/todos")]
+    public class TodosController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetTodos()
+        {
+            return Ok(TodosDataStore.Current.Todos);
+        }
+    }
+}
---
+++ Todos/TodosDataStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todos.Models;
+
+namespace Todos
+{
+    public class TodosDataStore
+    {
+        public static TodosDataStore Current { get; } = new TodosDataStore();
+        public List<TodoDto> Todos { get; set;  }
+
+        public TodosDataStore()
+        {
+            Todos = new List<TodoDto>()
+            {
+                new TodoDto()
+                {
+                    Id = 1,
+                    Title = "Walk the dog",
+                    IsCompleted = false
+                },
+                new TodoDto()
+                {
+                    Id = 2,
+                    Title = "Mow the yard",
+                    IsCompleted = false
+                },
+                new TodoDto()
+                {
+                    Id = 3,
+                    Title = "Clean the kitchen",
+                    IsCompleted = false
+                },
+            };
+        }
+
+    }
+}
